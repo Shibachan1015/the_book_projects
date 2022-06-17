422d9be271b4b528e572f13a5def7e46255b5ac9
Shibachan1015 <siba1015@cpost.plala.or.jp>
2022-06-17 21:05:16 +0900

    done variables

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,3 +1,5 @@
+use std::{io, usize};
+
 fn main() {
     let x = 5;
 
@@ -20,5 +22,28 @@ fn main() {
     println!("the value of x is: {}",x);
     println!("The value of y is: {}",y);
     println!("The value of z is: {}",z);
+
+    let a = [1, 2, 3, 4, 5];
+
+    println!("Please enter an array index.");
+           // 配列の何番目の要素にアクセスするか指定してください。
+
+    let mut index = String::new();
+
+    io::stdin()
+        .read_line(&mut index)
+        .expect("Failed to read line");
+    
+    let index: usize = index
+        .trim()
+        .parse()
+        .expect("Index enterd was not a number");
+
+    let element = a[index];
+
+    println!(
+        "The vale of the element at index {} is: {}",
+        index, element
+    );
     
 }
