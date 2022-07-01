c53546798d90296a1668ef16a377d7e14f87f164
Shibachan1015 <siba1015@cpost.plala.or.jp>
2022-07-02 00:33:02 +0900

    7-1

--- guessing_game/target/debug/deps/guessing_game-7586431a71ce01e9.d
+++ guessing_game/target/debug/deps/guessing_game-7586431a71ce01e9.d
@@ -1,5 +1,5 @@
-/Users/takashiba/rust/src/projects/guessing_game/target/debug/deps/guessing_game-7586431a71ce01e9.rmeta: src/main.rs
+/Users/takashiba/rust/src/the_book_projects/guessing_game/target/debug/deps/guessing_game-7586431a71ce01e9.rmeta: src/main.rs
 
-/Users/takashiba/rust/src/projects/guessing_game/target/debug/deps/guessing_game-7586431a71ce01e9.d: src/main.rs
+/Users/takashiba/rust/src/the_book_projects/guessing_game/target/debug/deps/guessing_game-7586431a71ce01e9.d: src/main.rs
 
 src/main.rs:
