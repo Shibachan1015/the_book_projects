64253706f0931cc7661115752d19821d6f14bd66
Shibachan1015 <siba1015@cpost.plala.or.jp>
2022-07-01 13:17:04 +0900

    think flow

--- conversion/target/debug/deps/conversion-1cc182a8c09556d5.d
+++ conversion/target/debug/deps/conversion-1cc182a8c09556d5.d
@@ -1,5 +1,6 @@
-/home/takao/rust/projects/the_book_projects/conversion/target/debug/deps/conversion-1cc182a8c09556d5: src/main.rs
+/home/takao/rust/projects/the_book_projects/conversion/target/debug/deps/conversion-1cc182a8c09556d5: src/main.rs src/my_calc.rs
 
-/home/takao/rust/projects/the_book_projects/conversion/target/debug/deps/conversion-1cc182a8c09556d5.d: src/main.rs
+/home/takao/rust/projects/the_book_projects/conversion/target/debug/deps/conversion-1cc182a8c09556d5.d: src/main.rs src/my_calc.rs
 
 src/main.rs:
+src/my_calc.rs:
--- conversion/target/debug/deps/conversion-fd01339a58f2ae2e.d
+++ conversion/target/debug/deps/conversion-fd01339a58f2ae2e.d
@@ -1,5 +1,6 @@
-/home/takao/rust/projects/the_book_projects/conversion/target/debug/deps/conversion-fd01339a58f2ae2e.rmeta: src/main.rs
+/home/takao/rust/projects/the_book_projects/conversion/target/debug/deps/conversion-fd01339a58f2ae2e.rmeta: src/main.rs src/my_calc.rs
 
-/home/takao/rust/projects/the_book_projects/conversion/target/debug/deps/conversion-fd01339a58f2ae2e.d: src/main.rs
+/home/takao/rust/projects/the_book_projects/conversion/target/debug/deps/conversion-fd01339a58f2ae2e.d: src/main.rs src/my_calc.rs
 
 src/main.rs:
+src/my_calc.rs:
