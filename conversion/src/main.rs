33576f1db060778680e7aa48b78b7ebb6ab71259
Shibachan1015 <siba1015@cpost.plala.or.jp>
2022-07-01 22:07:05 +0900

    4th

--- conversion/src/main.rs
+++ conversion/src/main.rs
@@ -2,6 +2,17 @@
 // °C = ((°F) -32) ÷ 1.8
 // °F = °C × 1.8 + 32
 
+    // 最終目標　println!("摂氏と華氏双方向に温度を変換できます。");
+    // exit()などで正常終了を実装したい。
+    // 次は華氏から摂氏に変換を実装する。
+    // |idea| if or match で　摂氏から華氏か華氏から摂氏を判定してそこでctof関数かftocを呼びだせば良いのではないか。
+    /*   print(choice c=1 or choice f=2)
+         | |---if choice c(){};
+         |      |---
+         |
+         |---if choice f(){};
+    */
+
 mod my_calc;  // calc_ctofなどの関数のモジュール
 use std::io;
 // use std::cmp::Ordering;
@@ -17,17 +28,19 @@ fn main() {
         +-------------------------------------------------+
         "
     );
-    // 最終目標　println!("摂氏と華氏双方向に温度を変換できます。");
-    // exit()などで正常終了を実装したい。
-    // 次は華氏から摂氏に変換を実装する。
-    // |idea| if or match で　摂氏から華氏か華氏から摂氏を判定してそこでctof関数かftocを呼びだせば良いのではないか。
-    /*   print(choice c=1 or choice f=2)
-         | |---if choice c(){};
-         |      |---
-         |
-         |---if choice f(){};
-    */
+    
+    let mut c_or_f = String::new();
 
+    io::stdin()
+        .read_line(&mut c_or_f)
+        .expect("行の読み込みに失敗しました。");
+
+    let c_or_f: f64 = match c_or_f.trim().parse() {
+        Ok(num) => num,
+        Err(_) => continue,
+    };
+
+/*---------------------------------------------------------------------------*/
     println!("摂氏を華氏に変換します。");
 
     loop {
